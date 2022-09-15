81df7860748b410e0c08a354853052f8fdd6a36e
AyoMoses1 <ayocandy1@gmail.com>
2022-09-14 18:52:44 -0700

    finished task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - takes two integer arguments and adds them together.
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result;
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
