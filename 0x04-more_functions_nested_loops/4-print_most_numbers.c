bf0fd6875aa53b97c038143edbbb8c309c5c9685
AyoMoses1 <ayocandy1@gmail.com>
2022-09-17 08:47:58 -0700

    finished task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,11 +5,11 @@
  */
 void print_most_numbers(void)
 {
-	char c = '0';
+	int c = 0;
 
-	while (c <= '9')
+	while (c <= 9)
 	{
-		if (c != '2' && c != '4')
+		if (c != 2 && c != 4)
 		{
 			_putchar(c);
 		}
