bce172e6d7c3207e84b3f8a467682e3cf7cf2346
AyoMoses1 <ayocandy1@gmail.com>
2022-10-25 02:03:44 -0700

    updated and checked for issues with task 12

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,32 +1,76 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a list
- * @h: the pointer to the start of list to free
- * Return: size of free'd list (in nodes?)
+ * detect_loop - Function that detects if is a loop
+ * @h: Head of the linked list
+ * Return: Number of length if is true or 0 if is false
+ */
+int detect_loop(listint_t *h)
+{
+	listint_t *slow, *fast;
+	int i;
+
+	slow = h, fast = h;
+
+	for (i = 1; slow && fast && fast->next; i++)
+	{
+		slow = slow->next, fast = fast->next->next;
+
+		if (slow == fast)
+			return (i);
+	}
+
+	return (0);
+}
+
+/**
+ * remove_loop - Function that removes loop in a linked list
+ * @h: Header of the linked list
+ * @n: lenth of the loop
+ */
+void remove_loop(listint_t **h, int n)
+{
+	listint_t *ptr1, *ptr2;
+
+	ptr1 = *h, ptr2 = *h;
+
+	for (; n > 0; n--)
+		ptr1 = ptr1->next;
+
+	while (1)
+	{
+		ptr1 = ptr1->next, ptr2 = ptr2->next;
+
+		if (ptr1->next == ptr2->next)
+		{
+			ptr1->next = NULL;
+			return;
+		}
+	}
+}
+
+/**
+ * free_listint_safe - Function that frees a linked list
+ * @h: Head of the linked list
+ * Return: The size of the list that was free
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t nodeCount = 0;
-	listint_t *temp = NULL;
+	listint_t *t;
+	int n;
 
-	if (!(h && *h))
-		return (nodeCount);
-	while (*h)
-	{
-		nodeCount++;
-		if (*h > (*h)->next)
-		{
-			temp = *h;
-			*h = (*h)->next;
-			free(temp);
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-		}
-	}
+	if (h == NULL || *h == NULL)
+		printf("0\n(nil)\n"), exit(98);
+
+	n = detect_loop(*h);
+
+	if (n != 0)
+		remove_loop(h, n);
+
+	for (n = 0; *h; n++)
+		t = *h, *h = (*h)->next, free(t);
+
 	*h = NULL;
-	return (nodeCount);
+
+	return (n);
 }
